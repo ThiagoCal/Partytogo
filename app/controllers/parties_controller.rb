1cd6463fd842189eac12410a695e53b1d3e8ff27
Thiago Calazans <thiagotorrescalazans@gmail.com>
2022-03-02 19:14:10 -0300

    parties controller and party model validations

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,2 +1,44 @@
 class PartiesController < ApplicationController
+  def index
+    @parties = Party.all
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @party = Party.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @party = Party.new(party_params)
+    if @party.save
+      redirect_to user_path(@user)
+    else
+      :new
+    end
+  end
+
+  def edit
+    @party = Party.find(params[:id])
+  end
+
+  def update
+    @party = party.find(params[:id])
+    if @party.update(party_params) #this will already save your nested project along with the party
+      redirect_to user_path(@party.user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @party = party.find(params[:id])
+    @party.destroy
+    redirect_to user_path(@party.user)
+  end
+
+  private
+    def party_params
+      require.params(:party).permit(:user_id, :name, :address, :date, :category, :description, :rating, :price, :capacity, :is_active)
+    end
 end
--- app/models/party.rb
+++ app/models/party.rb
@@ -4,4 +4,12 @@ class Party < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_one_attached :photo
   validates :photo, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :category, presence: true
+  validates :address, presence: true
+  validates :date, presence: true
+  validates :is_active, presence: true
+  validates :capacity, presence: true
 end
