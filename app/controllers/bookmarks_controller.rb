5738be66c977f98d083c1d60022f2a58b1b88ff6
Paulo Bazilio <pvbazilio@gmail.com>
2022-03-02 19:16:50 -0300

    Added bookmark controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,30 @@
 class BookmarksController < ApplicationController
+
+  before_action :set_bookmark, only: :destroy
+  before_action :set_party, only: [:new, :create]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def destroy
+    @bookmark.destroy
+
+  def create
+     @bookmark = Bookmark.new(bookmark_params)
+     @bookmark.party = @party
+
+    private
+
+    def bookmark_params
+      params.require(:bookmark).permit(:user_id, :party_id)
+    end
+
+    def set_bookmark
+      @bookmark = Bookmark.find(params[:id])
+    end
+
+    def set_party
+      @party = party.find(params[:party_id])
+    end
 end
