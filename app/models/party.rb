da1a86e7a8506a3758b5489bae2a801f7bd32d79
Thiago Calazans <thiagotorrescalazans@gmail.com>
2022-03-02 16:50:46 -0300

    created relations between models

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,3 +1,5 @@
 class Party < ApplicationRecord
   belongs_to :user
+  has_many :reviews
+  has_many :bookmarks
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :users do
+    resources :bookmarks, except:[:delete, :patch]
+    resources :parties, except:[:delete, :patch]
+  end
+  resources :parties, only:[:delete, :patch] do
+    resources :reviews, except:[:delete, :patch]
+  end
+  resources :bookmarks, only:[:delete]
+  resources :reviews, only:[:delete, :patch]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
