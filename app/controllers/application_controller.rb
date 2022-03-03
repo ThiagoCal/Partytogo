6340d3ddd80481031febc5eff90152231ea3ec8c
Thiago Calazans <thiagotorrescalazans@gmail.com>
2022-03-03 17:08:50 -0300

    fixed error in adding pic to the profile. Also created an edit page for the user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,10 +16,10 @@ class ApplicationController < ActionController::Base
   # end
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birthdate, :email, :is_producer, :photo)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birthdate, :email, :is_producer, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :birthdate, :email, :is_producer, :photo])
   end
 
   private
