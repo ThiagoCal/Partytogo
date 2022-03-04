943dd97622d6ade8df05ec1822ca456066e9c398
Thiago Calazans <thiagotorrescalazans@gmail.com>
2022-03-04 16:00:42 -0300

    all parties page

--- app/controllers/parties_display_controller.rb
+++ app/controllers/parties_display_controller.rb
@@ -15,4 +15,9 @@ class PartiesDisplayController < ApplicationController
     @party_display = Party.find(params[:id])
     authorize @party_display
   end
+
+  private
+    def party_params
+      params.require(:party).permit(:user_id, :name, :photo, :address, :date, :category, :description, :rating, :price, :capacity, :is_active)
+    end
 end
