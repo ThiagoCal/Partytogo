83872a0127b81b73272713d056f2e9ad97509dd7
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-03-07 17:30:19 -0300

    all parties done

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,10 +1,10 @@
 module ImageHelper
   def background_image(args = {})
-    url = args[:url]
+    url = 'https://henpartyitaly.com/wp-content/uploads/2017/12/party.jpg'
+
+    url = args[:url] unless args[:url].nil?
 
     url = cl_image_path url if args[:cloudinary]
-
-    url = 'https://henpartyitaly.com/wp-content/uploads/2017/12/party.jpg' if args[:url].nil?
 
     color_one = 'rgba(0, 0, 0, 0)'
     color_two = 'rgba(0, 0, 0, .1)'
