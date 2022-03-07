b15207b8418e294361df5cdc8b1683f95a4fcdbd
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-03-07 17:20:55 -0300

    parties index done

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -4,8 +4,10 @@ module ImageHelper
 
     url = cl_image_path url if args[:cloudinary]
 
+    url = 'https://henpartyitaly.com/wp-content/uploads/2017/12/party.jpg' if args[:url].nil?
+
     color_one = 'rgba(0, 0, 0, 0)'
-    color_two = 'rgba(0, 0, 0, .3)'
+    color_two = 'rgba(0, 0, 0, .1)'
 
     "background-image: linear-gradient(#{color_one}, #{color_two}), url(#{url})"
   end
