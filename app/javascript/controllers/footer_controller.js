cf294641f590b43a080543bb898d3e64bb662ba9
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-03-08 15:16:57 -0300

    footer done

--- app/javascript/controllers/footer_controller.js
+++ app/javascript/controllers/footer_controller.js
@@ -1,37 +1,34 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = {
-    usernames: Array
-  }
-
   static targets = ['github']
 
   connect() {
-    this.githubTarget.innerHTML = this.#userInfo();
+    this.userInfo();
+    console.log('hii')
   }
 
 
-  #fetchUserInfo(user) {
+  fetchUserInfo(user) {
     const url = `https://api.github.com/users/${user}`
 
     fetch(url)
       .then(response => response.json())
       .then((data) => {
-        user = data;
+        const html = this.htmlTemplate(data)
+        this.githubTarget.innerHTML += html
       })
-
-      return user;
   }
 
-  #userInfo() {
-    return this.usernamesValue.map((user) => {
-      const info = this.#fetchUserInfo(user)
-      this.#htmlTemplate(info)
+  userInfo() {
+    const usernames = ['ThiagoCal', 'magedeungaro', 'pvbazilio']
+
+    usernames.forEach((user) => {
+      this.fetchUserInfo(user)
     })
   }
 
-  #htmlTemplate(user){
-    return `<a href='${user.html_url}'><img src='${user.avatar_url}' alt='${user.login}' class='round-circle mx-2'></a>`
+  htmlTemplate(user){
+    return `<a href='${user.html_url}'><img src='${user.avatar_url}' alt='${user.login}' class='round-circle mx-3'></a>`
   }
 }
